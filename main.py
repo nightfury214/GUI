8676031c10c9ec505094f60975124f0175fd20b1
nightfury214 <207950995+nightfury214@users.noreply.github.com>
2023-09-11 21:13:55 +0100

    dummy show graph

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from ui import Ui, UiElement
 import pandas as pd ### not in requirements file - need to add this
+import matplotlib.pyplot as plt
+
+
+
+
 class State:
     patient = []
     control = [] 
     map_GPS = []
     ui=[]
+        
+    
+
 def load_patient_file(path):
     # code to load data goes here
     State.patient=pd.read_excel(path)
@@ -29,10 +37,21 @@ def calc_GPS():
     State.ui.set_element(UiElement.OUT_RGPS,str(GPS[0]))
     # end of your code
     # print(path)
+    
+def show_graph(path):
+    fig,ax = plt.subplots()
+
+    ax.plot(State.control.LKneeFlex_x)
+    ax.plot(State.patient.LKneeFlex_x)
+
+    State.ui.plot(fig)
 
 if __name__ == '__main__':
     ui = Ui()
+    State.ui=ui
     ui.add_select_callback(UiElement.SELECT_PATIENT, load_patient_file)
     ui.add_select_callback(UiElement.SELECT_CONTROL, load_control_file)
     ui.add_button_callback(UiElement.BUTTON_CALC, calc_GPS)
+    
+    ui.add_select_callback(UiElement.SELECT_GRAPH, show_graph)
     ui.mainloop()
