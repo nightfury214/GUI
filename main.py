a0ba356fcca47ed8384e6e4007bc82ec5dc64a17
nightfury214 <207950995+nightfury214@users.noreply.github.com>
2023-09-11 21:12:16 +0100

    dummy calculate GPS

--- main.py
+++ main.py
@@ -18,9 +18,21 @@ def load_control_file(path):
     print(State.control.LKneeFlex_x)
     # end of your code
     print(path)
+    
+def calc_GPS():
+    # code to load data goes here
+    # State.control=pd.read_excel(path)
+    GPS = State.patient.LKneeFlex_x - State.control.LKneeFlex_x
+    State.mapGPS = [5,9,3,5]
+    State.ui.set_element(UiElement.OUT_GPS,str(GPS[0]))
+    State.ui.set_element(UiElement.OUT_LGPS,str(GPS[0]))
+    State.ui.set_element(UiElement.OUT_RGPS,str(GPS[0]))
+    # end of your code
+    # print(path)
 
 if __name__ == '__main__':
     ui = Ui()
     ui.add_select_callback(UiElement.SELECT_PATIENT, load_patient_file)
     ui.add_select_callback(UiElement.SELECT_CONTROL, load_control_file)
+    ui.add_button_callback(UiElement.BUTTON_CALC, calc_GPS)
     ui.mainloop()
